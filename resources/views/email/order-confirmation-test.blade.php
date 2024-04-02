61a02e408d1ea7beea3f42d129526328c68fef17
Muhammad Hassan <f21bb126@ibitpu.edu.pk>
2024-04-02 15:09:33 +0500

    email

---
+++ resources/views/email/order-confirmation-test.blade.php
@@ -0,0 +1,5 @@
+<center>
+    <h2>Your Order has been confirmed with ID: <p class="sub-heading">{!! $data['shipping']['SO_Number'] == '' ? 'N/A' : $data['shipping']['SO_Number'] !!} </p></h2>
+    <br>
+</center>
+
