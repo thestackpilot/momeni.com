148b6a831c670768bc2c81fc4c64a17a78a16970
Sheikh Ammar Ahmad <sheikhammar568@gmail.com>
2024-05-22 15:34:20 +0500

    remove dump from cart

--- resources/views/frontend/MOM/components/menu.blade.php
+++ resources/views/frontend/MOM/components/menu.blade.php
@@ -168,7 +168,6 @@ use App\Http\Controllers\CommonController;
                                 </a>
                             </div>
                             <div class="col-md-9">
-                                @dump($item);
                                 <h3 class="font-ropa m-0">{{$item -> item_name}}</h3>
                                 {{--
                                 <p class="specs m-0"> <strong> Customer ID: </strong> <span> {{$item -> item_customer_id}} </span> </p>
