e7e96a22aeaebb4d37b11ab856e5fde16d2635a9
Sheikh Ammar Ahmad <sheikhammar568@gmail.com>
2024-09-26 17:28:20 +0500

    Add PLace order api success false condition check only

--- app/Http/Controllers/Frontend/CheckoutController.php
+++ app/Http/Controllers/Frontend/CheckoutController.php
@@ -480,7 +480,7 @@ class CheckoutController extends FrontendController
                     prr("Order Acknowledgment Email Exception :: " . $e->getMessage());
                 }
                 // }
-            } else {
+            } else if(!$result['Success']) {
                 $order_payment = $this->order_payment_model->updateOrCreate(
                     ['user_id' => Auth::user()->id, 'hash' => $order_payment_hash],
                     [
